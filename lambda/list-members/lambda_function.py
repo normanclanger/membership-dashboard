4a5b34bbd54574ddaa8112f5c3888b6faf0103a6
normanclanger <normanclanger@gmail.com>
2026-08-08 11:24:38 +0100

    Connect members Lambda to Supabase database

--- lambda/list-members/lambda_function.py
+++ lambda/list-members/lambda_function.py
@@ -1,30 +1,14 @@
+from database import get_connection
 from responses import success
 
 
 def lambda_handler(event, context):
 
-    members = [
-        {
-            "membership_number": "M001",
-            "first_name": "John",
-            "surname": "Smith"
-        },
-        {
-            "membership_number": "M002",
-            "first_name": "Jane",
-            "surname": "Brown"
-        },
-        {
-            "membership_number": "M003",
-            "first_name": "David",
-            "surname": "Wilson"
-        }
-    ]
+    conn = get_connection()
+    conn.close()
 
-    response = {
+    return success({
         "success": True,
-        "count": len(members),
-        "members": members
-    }
+        "message": "Database connection successful"
+    })
 
-    return success(response)
