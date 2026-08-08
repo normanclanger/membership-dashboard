230263acdbd2dc72f506b750ffd0ec7b357dc46f
normanclanger <normanclanger@gmail.com>
2026-08-08 11:46:17 +0100

    Download list from members Lambda from Supabase database

--- lambda/list-members/lambda_function.py
+++ lambda/list-members/lambda_function.py
@@ -5,10 +5,30 @@ from responses import success
 def lambda_handler(event, context):
 
     conn = get_connection()
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            SELECT
+                membership_number,
+                first_name,
+                surname
+            FROM members;
+        """)
+
+        rows = cur.fetchall()
+
     conn.close()
 
-    return success({
-        "success": True,
-        "message": "Database connection successful"
-    })
+    members = [
+        {
+            "membership_number": row[0],
+            "first_name": row[1],
+            "surname": row[2]
+        }
+        for row in rows
+    ]
 
+    return success(members)
+
+#if __name__ == "__main__":
+#    print(lambda_handler({}, {}))
